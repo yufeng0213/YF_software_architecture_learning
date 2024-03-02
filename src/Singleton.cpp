2ad089266b8648ba4fe0bacd0473cfc56fc6ace3
YuFeng <66428423+yufeng0213@users.noreply.github.com>
2024-03-02 22:49:59 +0800

    Update Singleton.cpp

--- src/Singleton.cpp
+++ src/Singleton.cpp
@@ -22,28 +22,33 @@ private:
 
 };
 
+// 懒汉式
 class Singleton2{
 private:
+    // 静态单例实例
     static Singleton2* instance;
-
+    // 确保线程安全的互斥锁
     static std::mutex m;
 
+    // 构造函数私有化
     Singleton2(){};
-
+    // 禁止拷贝构造和赋值操作
     Singleton2(const Singleton2&) = delete;
     Singleton2& operator=(const Singleton2&) = delete;
 
 public:
 
+    // 提供全局访问接口
     static Singleton2& getInstance(){
-        std::lock_guard<std::mutex> lock(m);
+        std::lock_guard<std::mutex> lock(m); // 保证线程安全
         if(instance == nullptr){
             instance = new Singleton2();
         }
-
         return *instance;
     }
 
+    // 可选的析构函数，如果需要删除单例应在程序退出时手动调用
+    // 注意： 在C++11之后，智能指针可以更好的管理单例资源
     ~Singleton2(){
         if(instance != nullptr){
 
