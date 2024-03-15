e43145a28c64a10c3820c6b474793055e05545c1
yufeng <yufeng05020213@gmail.com>
2024-03-15 23:37:52 +0800

    nlohman-json

--- src/thirdparty/nlohman-json/nlohman-json.cpp
+++ src/thirdparty/nlohman-json/nlohman-json.cpp
@@ -3,7 +3,7 @@
 //
 #include <iostream>
 #include "nlohmann/json.hpp"
-
+#include <fstream>
 using json = nlohmann::json;
 
 int main(){
@@ -22,6 +22,29 @@ int main(){
     };
 
     std::cout << j << std::endl;
+
+    // 写json到文件
+    std::ofstream file("test.json");
+    file<<j<<std::endl;
+    file.close();
+
+    // 反序列化json文件
+    // string to json
+    std::string s = "{\"answer\": 42, \"everything\": true}";
+    json j2 = json::parse(s);
+    std::cout<<j2.at("answer")<<std::endl;
+
+    // 后面要加"_json"
+    json j3 = "{\"happy\": true, \"pi\": 3.141}"_json;
+    std::cout<<j3.at("happy")<<std::endl;
+
+    // 从JSON对象转化为字节序列
+    std::string ss = j3.dump();
+    std::cout<<ss<<"\n";
+
+    // 以便于查看的方式将json对象写入文件
+    std::ofstream file2("test2.json");
+    file2<<std::setw(4)<<j3<<std::endl;
 
     return 0;
 }
