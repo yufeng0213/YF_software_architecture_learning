7cf94475754ac47e6e62b27f6bf44a9c8ff28aff
yufeng <yufeng05020213@gmail.com>
2024-03-21 22:32:07 +0800

    memory

--- src/tips/memory.cpp
+++ src/tips/memory.cpp
@@ -1,1 +1,17 @@
 #include <memory.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int *a = new int[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    int *b = new int[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &b[i]);
+    }
+    int *c = new int[n];
+    for (int i = 0; i < n; i++) {
+    }
+}
