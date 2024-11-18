f32e3f619b80657d08f72da7459cf64353ce5c68
yufeng <yufeng05020213@gmail.com>
2024-11-18 21:37:22 +0800

    Changes

---
+++ src/protobuf/testProto.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "dist/test.pb.h"
+
+int main(){
+
+    //
+    TEST::Quaternion q;
+    q.set_q0(0.2);
+    q.set_qx(0.1);
+    q.set_qy(0.9);
+    q.set_qz(0.8);
+    
+
+
+    return 0;
+}
